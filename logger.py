b0030394b237c24fadacb1fb722a9342466ba63b
RandiSPB <i.tsirulik@sellwell.ru>
2021-01-29 00:11:31 +0300

    Working with logger decorator

--- logger.py
+++ logger.py
@@ -1,11 +1,20 @@
 import cherrypy
+import json
 
 
 def logger_request(route_func):
 
-    def wrapper_func():
+    def wrapper_func(args):
+        route_func(args)
         print(route_func.__name__)
-        print(cherrypy.request)
-        route_func()
+        print(cherrypy.request.headers)
+        length = int(cherrypy.request.headers['content-length'])
+        print(f'lenght = {length}')
+        print('----------------')
+        json_string = cherrypy.request.body.read(length)
+        print(json_string)
+        print('-----------')
+        #print(f'json : \n{json.dumps(json_string)}')
+        print('---------')
 
     return wrapper_func
